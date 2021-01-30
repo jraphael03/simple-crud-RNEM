e71ffbb0373b73868a95cde3bd91186a35b3e400
Justin Raphael <justinraphael@Justins-MacBook-Air.local>
2021-01-30 13:11:01 -0500

    created a form with minor css

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="information">
+        <label htmlFor="">Name:</label>
+        <input type="text" />
+        <label htmlFor="">Age:</label>
+        <input type="number" />
+        <label htmlFor="">Country:</label>
+        <input type="text" />
+        <label htmlFor="">Position:</label>
+        <input type="text" />
+        <label htmlFor="">Wage (year):</label>
+        <input type="number" />
+        <button>Add Employee</button>
+      </div>
     </div>
   );
 }
