a4fb6e602289a4b126a7a6eace3f0f8d6827e38b
Justin Raphael <justinraphael@Justins-MacBook-Air.local>
2021-01-30 13:29:11 -0500

    Created useStates for the form and added onChange events for every input, can receive info

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [name, setName] = useState('');         //string
+  const [age, setAge] = useState(0);           //number
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  // Apply function to button and you can check that your code with onChange did work
+  // const displayInfo = () => {
+  //   console.log(name + age + country + position + wage);
+  // }
+
   return (
     <div className="App">
       <div className="information">
         <label htmlFor="">Name:</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => {setName(e.target.value)}} /> 
         <label htmlFor="">Age:</label>
-        <input type="number" />
+        <input type="number" onChange={(e) => {setAge(e.target.value)}} />
         <label htmlFor="">Country:</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => {setCountry(e.target.value)}} />
         <label htmlFor="">Position:</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => {setPosition(e.target.value)}} />
         <label htmlFor="">Wage (year):</label>
-        <input type="number" />
-        <button>Add Employee</button>
+        <input type="number" onChange={(e) => {setWage(e.target.value)}} />
+        <button  >Add Employee</button>
       </div>
     </div>
   );
