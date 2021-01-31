266a46ce3e6c8afed8177eaba66c0fabcd08bc86
Justin Raphael <justinraphael@Justins-MacBook-Air.local>
2021-01-31 12:48:19 -0500

    created update and delete functions for webpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
@@ -8,6 +9,69 @@ function App() {
   const [country, setCountry] = useState("");
   const [position, setPosition] = useState("");
   const [wage, setWage] = useState(0);
+  const [newWage, setNewWage] = useState(0);
+
+  const[employeeList, setEmployeeList] = useState([]);
+
+
+  // POST TO BACKEND FUNCTION
+  const addEmployee = () => {
+    //console.log(name);    //You can check error by console.log
+    axios.post("http://localhost:3001/create", {      //axios wants to send to the host you created and inside place the data you want to send 
+      name: name, 
+      age: age, 
+      country: country, 
+      position: position, 
+      wage: wage
+    }).then(() => {
+      setEmployeeList([             //Auto add employee to list without having to click the show employee button to see changes
+        ...employeeList,
+        {
+          name: name,
+          age: age,
+          country: country,
+          position: position,
+          wage: wage,
+        },
+      ]);
+      console.log("success");
+    })
+  };
+
+
+  // GET DATA FROM BACKEND FUNCTION
+  const getEmployees = () => {
+    axios.get("http://localhost:3001/employees",).then((response) => {      //varibale response will contain whatever is sent from the backend
+      //console.log(response)
+      setEmployeeList(response.data)    //grabbing the response above and data within setting it to employeeList
+    });
+  }
+
+
+  // UPDATE DATA FROM BACKEND
+  const updateEmployeeWage = (id) => {
+    axios.put("http://localhost:3001/update", {wage: newWage, id: id}).then(
+      (response) => {
+      //alert('update');
+      //UPDATE CLIENT SIDE IMMEDIATELY
+        setEmployeeList(employeeList.map((val) => {
+          return(
+            val.id == id ? {id: val.id, name: val.name, country: val.country, age: val.age, position: val.position, wage: newWage} : val         //is the user the same as the id we changed if so display new objects, else is isn't the user just pass val
+          )
+        }))
+    })
+  }
+
+
+  //DELETE DATA FROM BACKEND
+  const deleteEmployee = (id) => {
+    axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
+      //DELETE FROM CLIENT SIDE IMMEDIATELY
+      setEmployeeList(employeeList.filter((val) => {
+        return val.id !== id
+      }))
+    })
+  }
 
   // Apply function to button and you can check that your code with onChange did work
   // const displayInfo = () => {
@@ -18,16 +82,67 @@ function App() {
     <div className="App">
       <div className="information">
         <label htmlFor="">Name:</label>
-        <input type="text" onChange={(e) => {setName(e.target.value)}} /> 
+        <input
+          type="text"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
         <label htmlFor="">Age:</label>
-        <input type="number" onChange={(e) => {setAge(e.target.value)}} />
+        <input
+          type="number"
+          onChange={(e) => {
+            setAge(e.target.value);
+          }}
+        />
         <label htmlFor="">Country:</label>
-        <input type="text" onChange={(e) => {setCountry(e.target.value)}} />
+        <input
+          type="text"
+          onChange={(e) => {
+            setCountry(e.target.value);
+          }}
+        />
         <label htmlFor="">Position:</label>
-        <input type="text" onChange={(e) => {setPosition(e.target.value)}} />
+        <input
+          type="text"
+          onChange={(e) => {
+            setPosition(e.target.value);
+          }}
+        />
         <label htmlFor="">Wage (year):</label>
-        <input type="number" onChange={(e) => {setWage(e.target.value)}} />
-        <button  >Add Employee</button>
+        <input
+          type="number"
+          onChange={(e) => {
+            setWage(e.target.value);
+          }}
+        />
+        <button onClick={addEmployee}>Add Employee</button>
+      </div>
+
+      <div className="employees">
+        <button onClick={getEmployees} >Show Employees</button>
+
+          {employeeList.map((val, key) => {
+            return (
+              <div className="employee">
+                <div>
+                  <h3>Name: {val.name}</h3>
+                  <h3>Age: {val.age}</h3>
+                  <h3>Country: {val.country}</h3>
+                  <h3>Position: {val.position}</h3>
+                  <h3>Wage: {val.wage}</h3>
+                </div>
+                <div>
+                  <input type="text" placeholder="20000..." onChange={(event) => {
+                    setNewWage(event.target.value);
+                  }} />
+                  <button onClick={() => {updateEmployeeWage(val.id)}} >Update</button>
+                  <button onClick={() => {deleteEmployee(val.id)}} >Delete</button>
+                </div>
+              </div>
+            );
+          })}
+
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
 
 const app = express();
 const port = 3001;
+
+app.use(cors());
+app.use(express.json());
 
 // IMPORT MYSQL
 const db = mysql.createConnection({
     user: 'root',
     host: 'localhost',
-    pass: 'Opendoors744784',
+    password: 'Opendoors744784',
     database: 'employeeSystem',
 });
 
 
 // CREATING A POST REQUEST FOR DB
 app.post('/create', (req, res) => {
-    const name = req.body.name                  //request the body for variable name we want
+    //console.log(req.body)                     //Check what data is being passed to the backend
+    const name = req.body.name;                  //request the body for variable name we want
     const age = req.body.age;
     const country = req.body.country;
     const position = req.body.position;
@@ -31,6 +36,46 @@ app.post('/create', (req, res) => {
         }
     }        
     );
+})
+
+
+// CREATING A GET REQUEST FROM THE DB
+app.get('/employees' , (req, res) => {
+    db.query("SELECT * FROM employees", (err, result) => {
+        if (err) {
+            console.log(err)
+        } else{
+            res.send(result);
+        }
+    })
+})
+
+
+// CREATE AN APP FOR UPDATING THE DB
+app.put('/update', (req, res) => {
+    const id = req.body.id;
+    const wage = req.body.wage;
+    db.query("UPDATE employees SET wage = ? WHERE id = ?", [wage, id], (err, result) => {           // UPDATE (tablename) SET (row we want to update) WHERE (parameter used to choose update item) 
+        if(err){
+            console.log(err)
+        } else{
+            res.send(result);
+        }
+    });              
+});
+
+
+
+//CREATE AN APP TO DELETE FROM THE DB
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id
+    db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
+        if (err) {
+            console.log(err)
+        } else{
+            res.send(result);
+        }
+    })
 })
 
 
